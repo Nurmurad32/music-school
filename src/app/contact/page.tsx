a6654353d8b98f43f86b0adbddda363f12295e4f
Nurmurad32 <nur.murad32@gmail.com>
2024-05-01 22:37:44 +0600

    almost done

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactFormPage from '@/components/ContactFormPage'
+import React from 'react'
+
+function ContactPage() {
+  return (
+    <div><ContactFormPage /></div>
+  )
+}
+
+export default ContactPage
---
+++ src/app/courses/page.tsx
@@ -0,0 +1,11 @@
+import CourseCard from "@/components/CourseCard"
+
+function CoursesPage() {
+  return (
+    <div>
+        <CourseCard />
+    </div>
+  )
+}
+
+export default CoursesPage
